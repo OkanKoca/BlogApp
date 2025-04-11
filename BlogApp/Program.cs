66b2c8455f50fcc6194e34e9913061cd7b88f290
Okan Koca <b231210016@sakarya.edu.tr>
2025-04-11 14:15:57 +0300

    database changed from sqlite to postgresql

--- BlogApp/Program.cs
+++ BlogApp/Program.cs
@@ -1,24 +1,26 @@
 using BlogApp.Data.Concrete.EfCore;
 using Microsoft.EntityFrameworkCore;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container.  
 builder.Services.AddControllersWithViews();
+
 builder.Services.AddDbContext<BlogContext>(options =>
 {
     var config = builder.Configuration;
-    var connectionString = config.GetConnectionString("sql_connection");
-    options.UseSqlite(connectionString);
+    var connectionString = config.GetConnectionString("postgresql_connection");
+    options.UseNpgsql(connectionString);
 });
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline.  
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.  
     app.UseHsts();
 }
 
@@ -30,7 +32,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+   name: "default",
+   pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
